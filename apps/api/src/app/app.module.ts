c11f654648fb2beaccd786376ca7a05904f55c8e
vmrs418 <vaghanimit2@gmail.com>
2025-03-10 19:15:02 +0100

    Add API app.module.ts

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => databaseConfig,
+    }),
+    // Import your modules here
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
